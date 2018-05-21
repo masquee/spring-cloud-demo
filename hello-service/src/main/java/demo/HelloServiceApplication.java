5afb6e1413bd1c23ea802f3b7915c9fe152ebeda
masquee <cyclonee@163.com>
2018-05-21 20:51:53 +0800

    add hystrix-dashboard

--- hello-service/src/main/java/demo/HelloServiceApplication.java
+++ hello-service/src/main/java/demo/HelloServiceApplication.java
@@ -2,7 +2,9 @@ package demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
+@EnableHystrixDashboard
 @SpringCloudApplication
 public class HelloServiceApplication {
 
